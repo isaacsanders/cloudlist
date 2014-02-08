4b885221117ba859bd6c0018c7f2ca304a38e843
Francis <meng.francis@gmail.com>
2014-02-08 16:39:18 -0500

    fix party controller

--- models/Party.js
+++ models/Party.js
@@ -1,8 +1,11 @@
+var db = require('API_DB');
+
 function Party(party){
 	this.partyId = party.partyId,
 	this.partyName = party.partyName,
 	this.startDateTime = party.starteDateTime
 	this.location = party.location,
+	this.streetAddr = party.streetAddr,
 	this.duration = party.duration,
 	this.queueId = party.queueId
 }
@@ -10,9 +13,19 @@ function Party(party){
 module.exports = Party;
 
 Party.save = function(party, callback){
-
+	db.createEntity(party, function(err, party){
+		if(!err){
+			party.save(function(err){
+				if(err){
+					return callback(err);
+				}else{
+					return callback(err, party);
+				}
+			});
+		}
+	});
 };
 
 Party.get_party_with_location = function(location, callback){
 
-}
+};
--- routes/partyController.js
+++ routes/partyController.js
@@ -1,19 +1,22 @@
-ph = ph||{};
-ph = ph
+var ph = ph || {};
+ph.party_control = ph.party_control || {}
 
 
-exports.get_party = function(req, res){
+exports.get_party = function (req, res) {
 	return res.render('party', {title: "Party" });
 };
 
-exports.get_make_party = function(req, res){
+exports.get_make_party = function (req, res) {
 	return res.render('makeParty', {title: "New Party"});
 };
 
-exports.post_make_party = function(req, res){
-	req.body
+exports.post_make_party = function (req, res) {
+	var streetAddr = req.body.streetAddr;
+	var zipCode = req.body.zipCode;
+	var location = ph.party_control.get_location({streetAddr: streetAddr, zipCode: zipCode});
+
 };
 
-var get_location = function(location){
+ph.party_control.get_location = function (location) {
 
-}
+};
