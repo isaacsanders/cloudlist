1d2934a640571397058a2b8a567b2bf1fe6b1d61
Isaac Sanders <isaac@isaacbfsanders.com>
2014-02-08 13:18:41 -0500

    More stuff

--- public/js/addSong.js
+++ public/js/addSong.js
@@ -1,17 +1,5 @@
 (function($){
 	$(document).ready(function(){
-		var socket = io.connect('http://localhost:5000');
-		socket.on('hub:playlist', function(data){
-			console.log(data);
-		});
-		console.log('haha');
-		$('button.submit').click(function(){
-			var value = $('input.newSong').val();
-			var newSong = {};
-			newSong.name = value;
-			//put the vote in here
-			socket.emit('hub:playlist:update', {newSong:newSong});
-		});
 	})
 
 })(jQuery);
