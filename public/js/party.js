7e2bb3ccb018e73f805cff19ccb78df3b7699131
Chadwick Jones <jonescd@rose-hulman.edu>
2014-02-08 11:03:29 -0500

    Partayyyy page

--- public/js/party.js
+++ public/js/party.js
@@ -5,9 +5,11 @@
 		socket.on('client:playlist', function(data){
 			console.log(data.songList);
 			$('.songName').remove();
+			$('.songContainer').remove();
 			for(var i = 0; i <data.songList.length;i++){
-
 				$('#musicPane').append('<p class = "songName">'+data.songList[i].name+'</p>');
+
+				$('#queueContainer').append('<div class="songContainer"><div class="queueNumber">'+(i+1)+'</div><img class="queueAlbum" src="/resources/images/chance.jpg" /><p class="upVotes">'+'10'+'</p><img class="thumbUp" src="/resources/images/thumbUp.svg" /><p class = "songTitle">'+data.songList[i].name+'</p><p class="downVotes">'+'0'+'</p><img class="thumbDown" src="/resources/images/thumbDown.svg" /></div></div>');
 			}
 
 		});
