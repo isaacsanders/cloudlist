d564c3c3994caa5fd21951667b4a860f2e1b6fb2
Isaac Sanders <isaac@isaacbfsanders.com>
2014-02-09 05:33:40 -0500

    Mergy

--- public/js/hub.js
+++ public/js/hub.js
@@ -45,17 +45,18 @@
     var socket = io.connect('http://localhost:5000');
 
     socket.on('hub:playlist', function(songlist){
-	    console.log(songlist);
+      var duration;
       if (!window.song) {
         if (songlist.length > 0) {
          var singleSong = songlist.shift();
-	      window.song = singleSong.song;
+         window.song = singleSong.song;
           socket.emit('hub:playlist:dequeue', null);
-          var duration;
           SC.get("/tracks/"+window.song.trackId, function(sound) {
             duration = Math.floor(sound.duration / 1000);
           });
           $('#mainPlayer').html(playerTemplate(window.song));
+//             $('#mainPlayer .playerAlbum').attr('src', window.song.albumArtworkUrl);
+//             $('#mainPlayer .songInfo').text(window.song.title+" - "+window.song.artist);
           window.updatePlayerState(0, duration, true);
         } else {
           // Currently, do nothing if there are no songs
@@ -66,30 +67,52 @@
           $('.admin').on('click', '#playButton', function(){
             $('#playButton').toggle();
             $('#pauseButton').toggle();
-            SC.stream("/tracks/"+window.song.trackId, function(sound) {
-              console.log(sound);
-              sound.play();
+            SC.stream("/tracks/"+window.song.trackId, {
+              autoPlay: true
+            }, function(sound) {
               socket.emit('hub:nowPlaying', window.song);
-              $('.admin').on('click', '#playButton', function() {
-                $('#playButton').toggle();
-                window.updatePlayerState(window.songTime.progress, duration, true);
-                $('#pauseButton').toggle();
-                sound.play();
+              $('.admin').on('click', '.playButton', function() {
+                $('.playButton').toggleClass('playButton pauseButton');
+                window.updatePlayerState(sound.position, duration, true);
+                if (sound.paused) {
+                  sound.resume();
+                } else {
+                  sound.play();
+                }
               });
-              $('.admin').on('click', '#pauseButton', function() {
-                $('#pauseButton').toggle();
-                window.updatePlayerState(window.songTime.progress, duration, false);
-                $('#playButton').toggle();
+              $('.admin').on('click', '.pauseButton', function() {
+                $('.pauseButton').toggleClass('playButton pauseButton');
+                window.updatePlayerState(sound.position, duration, false);
                 sound.pause();
               });
             });
           });
         }
       }
-	  var musicList= [];
-	    for(var i =0; i< songlist.length;i++){
-		    musicList.push(songlist[i].song);
-	    }
+//       SC.stream("/tracks/"+window.song.trackId, {
+//         autoPlay: window.isPlaying,
+//         onfinish: function() {
+//           if (songlist.length > 0) {
+//             window.song = songlist.shift();
+//             window.isPlaying = true;
+//           } else {
+//             window.isPlaying = false;
+//           }
+//         }
+//       }, function(sound) {
+//         $('.admin').on('click', '#playButton', function() {
+//           if (!window.isPlaying) {
+//             window.updatePlayerState(window.songTime.progress, duration, true);
+//             sound.play();
+//             window.isPlaying = true;
+//           }
+//         });
+//       });
+
+      var musicList= [];
+      for(var i =0; i< songlist.length;i++){
+        musicList.push(songlist[i].song);
+      }
 
       var playlistHtml = $.map(musicList, function(track) {
         return songTemplate(track);
