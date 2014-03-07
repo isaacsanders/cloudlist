87418bbb4429f33eaf46a27976e67941817ab12b
shahdk <shahdk@rose-hulman.edu>
2014-03-06 22:00:14 -0500

    the screen titles are not center of screen regardless of size

--- CloudList/gen/com/example/cloudlist/R.java
+++ CloudList/gen/com/example/cloudlist/R.java
@@ -31,18 +31,19 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
-        public static final int button1=0x7f090005;
-        public static final int button2=0x7f090008;
+        public static final int action_settings=0x7f09000c;
+        public static final int button1=0x7f090006;
+        public static final int button2=0x7f090007;
         public static final int content_frame=0x7f090001;
         public static final int drawer_layout=0x7f090000;
+        public static final int layout1=0x7f090003;
         public static final int left_drawer=0x7f090002;
-        public static final int textView1=0x7f090007;
-        public static final int textView2=0x7f090006;
-        public static final int textViewCloud=0x7f090003;
-        public static final int textViewCloud1=0x7f090009;
+        public static final int textView1=0x7f090009;
+        public static final int textView2=0x7f090008;
+        public static final int textViewCloud=0x7f090005;
+        public static final int textViewCloud1=0x7f09000a;
         public static final int textViewJoin=0x7f090004;
-        public static final int textViewStart=0x7f09000a;
+        public static final int textViewStart=0x7f09000b;
     }
     public static final class layout {
         public static final int cloud_list_main=0x7f030000;
